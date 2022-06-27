dca1fc5ca3e8c920d04b4c783d1e4e285f950b1e
Kunle Falayi <oaf2125@columbia.edu>
2022-06-26 21:54:08 -0400

    Add files via upload

--- us-wildfire-risks/map.js
+++ us-wildfire-risks/map.js
@@ -100,7 +100,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibGV4eWFydGh1ciIsImEiOiJja3gwc3M3dHoxN3ByMnZte
 var map2 = new mapboxgl.Map({
     container: 'map2',
     style: 'mapbox://styles/lexyarthur/cl4w0r1gk004714p55fnxd94y', 
-    zoom: 4.8,
+    zoom: 5,
     maxZoom: 9,
     minZoom: 2.5,
     center: [-120.818, 37.024],
@@ -129,7 +129,7 @@ map2.on('load', function () {
         type: "geojson",
         data: "data/caliData.geojson",
         },
-        maxzoom: 5,
+        maxzoom: 6,
         paint: {
         "fill-color": '#fc4e2a',
         "fill-opacity": 0.75
@@ -145,7 +145,7 @@ map2.on('load', function () {
             type: "geojson",
             data: "data/caliCounties.geojson",
           },
-          minzoom: 5,
+          minzoom: 6,
           paint: {
             "line-color": "#ffffff",
             "line-width": 0.25,
@@ -164,7 +164,7 @@ map2.on('load', function () {
             type: "geojson",
             data: "data/caliCounties.geojson",
           },
-          minzoom: 5,
+          minzoom: 6,
           paint: {
             "fill-color": [
                 'interpolate',
